e3fdd9d13cd3d6a0a172b313a40fd62d38811929
吴梦荣 <wumengrong@yiliangche.net>
2017-01-24 15:58:05 +0800

    add modules service and providers

--- app/Modules/Blog/Http/Controllers/IndexController.php
+++ app/Modules/Blog/Http/Controllers/IndexController.php
@@ -9,5 +9,62 @@ use App\Http\Controllers\Controller;
 
 class IndexController extends Controller
 {
-    //
+
+    /**
+     * bolg list
+     * @param Request $request
+     */
+    public function index(Request $request){
+
+    }
+
+    /**
+     * add a blog
+     * @param Request $request
+     */
+    public function getAddBlog(Request $request){
+
+    }
+
+    /**
+     * store a new blog
+     * @param Request $request
+     */
+    public function postAddBlog(Request $request){
+
+    }
+
+    /**
+     * edit a blog
+     * @param Request $request
+     */
+    public function getEditBlog(Request $request){
+
+    }
+
+    /**
+     * store the result of edit a blog
+     * @param Request $request
+     */
+    public function postEditBlog(Request $request){
+
+    }
+
+
+    /**
+     * verify each option of your new add blog
+     * @param $obj
+     */
+    public function checkAddBlog($obj){
+
+    }
+
+    /**
+     * verify each option of your edit blog
+     * @param $obj
+     */
+    public function checkEditBlog($obj){
+
+    }
+
 }
