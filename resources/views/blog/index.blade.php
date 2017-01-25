a54c2f1251a7f88e7cbecb629df8fcdf0a70f53c
吴梦荣 <wumengrong@yiliangche.net>
2017-01-25 09:05:13 +0800

    add parsedown.php and Ueditor.vue

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -4,9 +4,7 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}">
         <link rel="stylesheet" href="{{asset('css/FontAwesome/font-awesome.min.css')}}">
-        <link rel="stylesheet" href="http://cdn.static.runoob.com/libs/bootstrap/3.3.7/css/bootstrap.min.css">
         <title>Laravel</title>
 
 
