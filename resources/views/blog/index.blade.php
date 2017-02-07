65d27edaa0bee25a7914a9104d1b7489a5b0a469
吴梦荣 <wumengrong@yiliangche.net>
2017-02-07 09:33:16 +0800

    add jstree directory

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -30,56 +30,73 @@
         <aside id="sidebar" class="sticky" style="top: 20px;">
 
             <div class="inside">
+                <div id="jstree_demo_div">
 
+                </div>
                 <h2>Title 1</h2>
-
                 <p>Lorem ipsum enim a mi pulvinar aliquet.</p>
-
-                <h2>Title 2</h2>
-
-                <p>Lorem a sollicitudin tempor.  </p>
-
             </div>
 
         </aside>
 
- 
+
         {{--baidu condensed or compressed version--}}
         <script src="http://libs.baidu.com/jquery/1.10.2/jquery.min.js"></script>
         <script src="{{asset('js/stickySidebar/stickySidebar.js')}}"></script>
        <link rel="stylesheet" href="{{asset('css/stickySidebar/style.css')}}">
+       {{--Side navigation http://www.htmleaf.com/jQuery/Menu-Navigation/201503011440.html--}}
+        <script type="text/javascript">
+            $(document).ready(function () {
+                $('#sidebar').stickySidebar();
+            })
+        </script>
 
-    <script type="text/javascript">
-        $(document).ready(function () {
-            $('#sidebar').stickySidebar();
-        })
-    </script>
+       {{--put the title into the side navigation--}}
+        <script type="text/javascript">
+            var i = 0;
+            var str = '';
+            var s = null;
+            while (1) {
+                s = $(":header").eq(i).text();
 
-<script type="text/javascript">
-var i=0;
-var str='';
-var s=null;
-while(1){
- s=$(":header").eq(i).text();
+                if (s == null) {
+                    break;
+                }
+                if (typeof(s) == "undefined") {
+                    break;
+                }
+                if (s == "") {
+                    break;
+                }
 
-if(s==null){
-console.log('null');
-break;
-}
-if(typeof(s) == "undefined"){
-break;
-}
-if(s==""){
-break;
-}
+                str = str + '<a href="#' + i + '">' + s + '</a>';
+                i++;
+            }
 
-  str=str+'<a href="#'+i+'">'+s+'</a>\n';
-  i++;
+            $('#jstree_demo_div').html(str);
+        </script>
 
-}
-console.log(str);
-$('.inside').html(str);
-</script>
+        {{--jstrss file https://www.jstree.com/--}}
+        <link rel="{{asset('jsTree/dist/themes/default-dark/style.min.css')}}">
+        <script src="{{asset('jsTree/jstree.min.js')}}"></script>
+        <script type="text/javascript">
+            $(function () {
+                //create an instance when the dom is ready
+                $('#jstree_demo_div').jstree();
+                //bind to events triggered on the tree
+                $('#jstree_demo_div').on("changed.jstree", function (e, data) {
+                    console.log(data.selected);
+                });
+                //interact with the tree-either way is ok
+                $('button').on('click', function () {
+                    $('#jstree').jstree(true).select_node('child_node_1');
+                    $('#jstree').jstree('select_node', 'child_node_1');
+                    $.jstree.reference('#jstree').select_node('child_node_1');
+                });
+            });
+
+
+        </script>
 
     </body>
 </html>
