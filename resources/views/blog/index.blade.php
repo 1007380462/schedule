3fa25980439993c38350c27727b6c9aa3726aba1
root <root@localhost.localdomain>
2017-02-08 08:40:52 +0800

    complete sidebar

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -104,14 +104,40 @@
         </script>
 
         {{--fancytree--}}
+<!--
         <script src="//code.jquery.com/jquery-1.12.1.min.js"></script>
         <script src="//code.jquery.com/ui/1.11.4/jquery-ui.min.js"></script>
-        <link href="{{asset('fancytree/dist/skin-win8/ui.fancytree.min.css')}}" rel="stylesheet">
+
+        <link href="{{asset('fancytree/dist/skin-lion/ui.fancytree.min.css')}}" rel="stylesheet">
         <script src="{{asset('fancytree/dist/jquery.fancytree-all.min.js')}}"></script>
+        <script src="{{asset('fancytree/dist/src/jquery.fancytree.glyph.js')}}"></script>
+
+      <script src="{{asset('fancytree/dist/src/jquery.fancytree.js')}}"></script>
+      <script src="{{asset('fancytree/dist/src/jquery.fancytree.dnd.js')}}"></script>
+     <script src="{{asset('fancytree/dist/src/jquery.fancytree.edit.js')}}"></script>
+-->
+
+<script src="//code.jquery.com/jquery-1.12.1.min.js"></script>
+  <script src="//code.jquery.com/ui/1.11.4/jquery-ui.min.js"></script>
+  <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+  <script src="//netdna.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+
+  <link href="{{asset('fancytree/dist/skin-bootstrap/ui.fancytree.css')}}" rel="stylesheet" class="skinswitcher">
+
+  <script src="{{asset('fancytree/dist/src/jquery.fancytree.js')}}"></script>
+  <script src="{{asset('fancytree/dist/src/jquery.fancytree.dnd.js')}}"></script>
+  <script src="{{asset('fancytree/dist/src/jquery.fancytree.edit.js')}}"></script>
+  <script src="{{asset('fancytree/dist/src/jquery.fancytree.glyph.js')}}"></script>
+  <script src="{{asset('fancytree/dist/src/jquery.fancytree.table.js')}}"></script>
+  <script src="{{asset('fancytree/dist/src/jquery.fancytree.wide.js')}}"></script>
+
+<!--
         <script type="text/javascript">
+
             $(function(){
                 // Create the tree inside the <div id="tree"> element.
                 $("#tree").fancytree({
+               extensions: ["dnd", "edit"],
                     source:[
                         {title: "Node 1", key: "1"},
                         {title: "Folder 2", key: "2", folder: true, children: [
@@ -120,9 +146,157 @@
                         ]}
                     ],
                     checkbox:true,
+
+dnd: {
+        autoExpandMS: 400,
+        focusOnClick: true,
+        preventVoidMoves: true, // Prevent dropping nodes 'before self', etc.
+        preventRecursiveMoves: true, // Prevent dropping nodes on own descendants
+        dragStart: function(node, data) {
+          /** This function MUST be defined to enable dragging for the tree.
+           *  Return false to cancel dragging of node.
+           */
+          return true;
+        },
+        dragEnter: function(node, data) {
+          /** data.otherNode may be null for non-fancytree droppables.
+           *  Return false to disallow dropping on node. In this case
+           *  dragOver and dragLeave are not called.
+           *  Return 'over', 'before, or 'after' to force a hitMode.
+           *  Return ['before', 'after'] to restrict available hitModes.
+           *  Any other return value will calc the hitMode from the cursor position.
+           */
+          // Prevent dropping a parent below another parent (only sort
+          // nodes under the same parent)
+/*           if(node.parent !== data.otherNode.parent){
+            return false;
+          }
+          // Don't allow dropping *over* a node (would create a child)
+          return ["before", "after"];
+*/
+           return true;
+        },
+        dragDrop: function(node, data) {
+          /** This function MUST be defined to enable dropping of items on
+           *  the tree.
+           */
+          data.otherNode.moveTo(node, data.hitMode);
+        }
+      },
+      activate: function(event, data) {
+//        alert("activate " + data.node);
+      },
+      lazyLoad: function(event, data) {
+        data.result = {url: "ajax-sub2.json"}
+      }
+
                 });
             });
         </script>
+-->
+<style type="text/css">
+  /* Define custom width and alignment of table columns */
+  #treetable {
+    table-layout: fixed;
+  }
+  #treetable tr td:nth-of-type(1) {
+    text-align: right;
+  }
+  #treetable tr td:nth-of-type(2) {
+    text-align: center;
+  }
+  #treetable tr td:nth-of-type(3) {
+    min-width: 100px;
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+  }
+</style>
+<script type="text/javascript">
+  glyph_opts = {
+    map: {
+      doc: "glyphicon glyphicon-file",
+      docOpen: "glyphicon glyphicon-file",
+      checkbox: "glyphicon glyphicon-unchecked",
+      checkboxSelected: "glyphicon glyphicon-check",
+      checkboxUnknown: "glyphicon glyphicon-share",
+      dragHelper: "glyphicon glyphicon-play",
+      dropMarker: "glyphicon glyphicon-arrow-right",
+      error: "glyphicon glyphicon-warning-sign",
+      expanderClosed: "glyphicon glyphicon-menu-right",
+      expanderLazy: "glyphicon glyphicon-menu-right",  // glyphicon-plus-sign
+      expanderOpen: "glyphicon glyphicon-menu-down",  // glyphicon-collapse-down
+      folder: "glyphicon glyphicon-folder-close",
+      folderOpen: "glyphicon glyphicon-folder-open",
+      loading: "glyphicon glyphicon-refresh glyphicon-spin"
+    }
+  };
+  $(function(){
+    // Initialize Fancytree
+    $("#tree").fancytree({
+      extensions: ["dnd", "edit", "glyph", "wide"],
+      checkbox: true,
+      dnd: {
+        focusOnClick: true,
+        dragStart: function(node, data) { return true; },
+        dragEnter: function(node, data) { return false; },
+        dragDrop: function(node, data) { data.otherNode.copyTo(node, data.hitMode); }
+      },
+      glyph: glyph_opts,
+      selectMode: 2,
+      source:[
+                        {title: "Node 1", key: "1"},
+                        {title: "Folder 2", key: "2", folder: true, children: [
+                            {title: "Node 2.1", key: "3"},
+                            {title: "Node 2.2", key: "4"}
+                        ]}
+                    ],
+      toggleEffect: { effect: "drop", options: {direction: "left"}, duration: 400 },
+      wide: {
+        iconWidth: "1em",     // Adjust this if @fancy-icon-width != "16px"
+        iconSpacing: "0.5em", // Adjust this if @fancy-icon-spacing != "3px"
+        levelOfs: "1.5em"     // Adjust this if ul padding != "16px"
+      },
 
+      icon: function(event, data){
+        // if( data.node.isFolder() ) {
+        //   return "glyphicon glyphicon-book";
+        // }
+      },
+      lazyLoad: function(event, data) {
+        data.result = {url: "ajax-sub2.json", debugDelay: 1000};
+      }
+    });
+
+    $("#treetable").fancytree({
+      extensions: ["dnd", "edit", "glyph", "table"],
+      checkbox: true,
+      dnd: {
+        focusOnClick: true,
+        dragStart: function(node, data) { return true; },
+        dragEnter: function(node, data) { return true; },
+        dragDrop: function(node, data) { data.otherNode.copyTo(node, data.hitMode); }
+      },
+      glyph: glyph_opts,
+      source: {url: "ajax-tree-taxonomy.json", debugDelay: 1000},
+      table: {
+        checkboxColumnIdx: 1,
+        nodeColumnIdx: 2
+      },
+
+      activate: function(event, data) {
+      },
+      lazyLoad: function(event, data) {
+        data.result = {url: "ajax-sub2.json", debugDelay: 1000};
+      },
+      renderColumns: function(event, data) {
+        var node = data.node,
+          $tdList = $(node.tr).find(">td");
+        $tdList.eq(0).text(node.getIndexHier());
+        $tdList.eq(3).text(!!node.folder);
+      }
+    });
+  });
+</script>
     </body>
 </html>
